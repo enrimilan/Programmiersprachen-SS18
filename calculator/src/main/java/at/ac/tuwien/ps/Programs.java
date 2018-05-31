ef9fd738de605767a7483510082635f1da2fa371
Enri Miho <e0929003@student.tuwien.ac.at>
2018-05-31 19:19:56 +0200

    add some predefined programs

---
+++ calculator/src/main/java/at/ac/tuwien/ps/Programs.java
@@ -0,0 +1,13 @@
+package at.ac.tuwien.ps;
+
+/**
+ * A set of predefined programs
+ */
+public class Programs {
+
+    public static final String RUN_PROGRAM = "((Welcome!)1w((Please write your program or enter the name of a predefined one (conditional|factorial|prime):)1w1ra0ra)0w0ra)";
+    public static final String CONDITIONAL = "(0 (9~)(8)(3c4d1+da)a1w)";
+    public static final String FACTORIAL = "((Enter number:)1w1r(2c1 3c-1c1=3c()(3c4d1+da)a2d*)2c3d2ca2d1w)";
+    public static final String PRIME = "";//TODO
+
+}
