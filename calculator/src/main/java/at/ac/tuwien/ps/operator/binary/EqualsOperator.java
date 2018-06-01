60c2f5e3bdc55165b95a4f17c259d059d3c48463
Enri Miho <e0929003@student.tuwien.ac.at>
2018-06-01 21:49:48 +0200

    check for the size of the stack

--- calculator/src/main/java/at/ac/tuwien/ps/operator/binary/EqualsOperator.java
+++ calculator/src/main/java/at/ac/tuwien/ps/operator/binary/EqualsOperator.java
@@ -4,6 +4,7 @@ import at.ac.tuwien.ps.Context;
 import at.ac.tuwien.ps.element.Element;
 import at.ac.tuwien.ps.element.ElementType;
 import at.ac.tuwien.ps.operator.Operator;
+import at.ac.tuwien.ps.operator.OperatorException;
 
 import java.util.Stack;
 
@@ -15,6 +16,9 @@ public class EqualsOperator implements Operator {
     @Override
     public void execute(Context context) {
         Stack<Element> stack = context.getDataStack();
+        if(stack.size() < 2)
+            throw new OperatorException("Error at " + this.getClass().getSimpleName() + " -> Stack needs to contain at least 2 elements but has " + stack.size());
+
         String first = stack.pop().getValue();
         String second = stack.pop().getValue();
         String result = first.equals(second)? "1" : "0";
