d90fc73c1869c97d567d47d4453b947d375aca1e
Enri Miho <e0929003@student.tuwien.ac.at>
2018-06-01 21:59:01 +0200

    cleanup and return directly a string

--- calculator/src/main/java/at/ac/tuwien/ps/operator/binary/GreaterThanOperator.java
+++ calculator/src/main/java/at/ac/tuwien/ps/operator/binary/GreaterThanOperator.java
@@ -6,21 +6,19 @@ import at.ac.tuwien.ps.Context;
 import at.ac.tuwien.ps.Pair;
 import at.ac.tuwien.ps.element.Element;
 import at.ac.tuwien.ps.element.ElementType;
-import at.ac.tuwien.ps.operator.OperatorException;
 
 /**
  * Takes two integers from the data stack and pushes the result of (first > second) onto the data stack.
  * An error is reported if one of the two topmost elements on the data stack is not an integer.
  */
-
 public class GreaterThanOperator extends BinaryOperator {
 
     @Override
     public void execute(Context context) {
     	Pair<Integer,Integer> pair = checkForIntegers(context);
     	Stack<Element> stack = context.getDataStack();
-    	int result = (pair.x > pair.y) ? 1 : 0;
-    	stack.push(new Element(String.valueOf(result), ElementType.INTEGER));
+    	String result = (pair.x > pair.y) ? "1" : "0";
+    	stack.push(new Element(result, ElementType.INTEGER));
     }
 
 }
