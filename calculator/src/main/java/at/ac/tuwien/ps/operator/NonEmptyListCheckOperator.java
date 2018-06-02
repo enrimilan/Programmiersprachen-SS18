7b0bb8e124e956e31158fad6ec0927c674c93853
Enri Miho <e0929003@student.tuwien.ac.at>
2018-06-02 09:39:14 +0200

    fix non empty list check operator and test it

--- calculator/src/main/java/at/ac/tuwien/ps/operator/NonEmptyListCheckOperator.java
+++ calculator/src/main/java/at/ac/tuwien/ps/operator/NonEmptyListCheckOperator.java
@@ -15,14 +15,18 @@ public class NonEmptyListCheckOperator implements Operator {
     @Override
     public void execute(Context context) {
     	Stack<Element> stack = context.getDataStack();
+		if(stack.size() < 1)
+			throw new OperatorException("Error at " + this.getClass().getSimpleName() + " -> Stack needs to contain at least 1 element but has " + stack.size());
+
     	Element top = stack.peek();
     	if(top.getElementType() == ElementType.LIST){
-    		String data = top.getValue();
-    		for(int i=0; i<data.length(); i++) {
-                if((data.charAt(i) != '(') && (data.charAt(i) != ')')) {
-                	stack.push(new Element(String.valueOf(1), ElementType.INTEGER));
-                }
-    		}
+    		String list = top.getValue();
+    		if(list.length() == 2 && list.charAt(0) == '(' && list.charAt(1) == ')') {
+				stack.push(new Element(String.valueOf(0), ElementType.INTEGER));
+			}
+			else {
+				stack.push(new Element(String.valueOf(1), ElementType.INTEGER));
+			}
     		return;
     	}
     	stack.push(new Element(String.valueOf(0), ElementType.INTEGER));
