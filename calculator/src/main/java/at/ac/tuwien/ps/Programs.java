385fcf86bc8cadfcf8800ac84ecb9ae7ad7bfaf6
Enri Miho <e0929003@student.tuwien.ac.at>
2018-09-09 21:23:57 +0200

    fix a space bug

--- calculator/src/main/java/at/ac/tuwien/ps/Programs.java
+++ calculator/src/main/java/at/ac/tuwien/ps/Programs.java
@@ -7,7 +7,7 @@ public class Programs {
 
     public static final String RUN_PROGRAM = "((Welcome!\n)1w((Please write your program or enter the name of a predefined one(conditional|factorial|prime):\n)1w1ras3+(3c3<(1w(\n)1w1d1d)(3cc:1 4c- 3c3c4d4d4d2ca)(3c4d1+da)a)()2ca0ra)0w0ra)";
     public static final String CONDITIONAL = "(0(9~)(8)(3c4d1+da)a1w(\n)1w)";
-    public static final String FACTORIAL = "((Enter number:\n)1w1ra(2c1 3c-1c1=3c()(3c4d1+da)a2d*)2c3d2ca2d1w(\n)1w)";
+    public static final String FACTORIAL = "((Enter number:\n)1w1ra(2c1 3c- 1c1=3c()(3c4d1+da)a2d*)2c3d2ca2d1w(\n)1w)";
 
     /*
     Prime factor program explained:
